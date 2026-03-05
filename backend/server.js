caca8ccd372b6d1d75cf7ae3e7b7beb4e302b21b
Rahulsw047 <rahulgangwani79@gamil.com>
2026-03-05 19:43:58 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/items',require('./routes/itemRoutes'));
 app.use('/api/work-entries',require('./routes/workRoutes'));
 app.use('/api/auth',require('./routes/authRoutes'));
 
-const PORT=5000;
+const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 });
