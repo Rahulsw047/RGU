cbefa5690beb485ebe33141e46ff6d10acc82323
Rahulsw047 <rahulgangwani79@gamil.com>
2026-03-07 00:15:09 +0530

    api.js updated

--- frontend/api.js
+++ frontend/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API=axios.create({
-    baseURL: 'https://rgu-4h9w.onrender.com',
+    baseURL: 'https://rgu-4h9w.onrender.com'
 });
 
 API.interceptors.request.use((req)=>{
